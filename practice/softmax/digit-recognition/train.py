0f2254c7140de01bba7f834afb6d30638f28165e
Niraj Mahajan <nirajmahajan007@gmail.com>
2019-12-15 06:52:46 +0530

    Changed digit reco from softmax

--- practice/softmax/digit-recognition/train.py
+++ practice/softmax/digit-recognition/train.py
@@ -1,4 +1,10 @@
 from helpers import *
+import argparse
+import os
+import pickle
+
+parser = argparse.ArgumentParser(description='Image Detection')
+parser.add_argument('-use_trained_model', action = 'store_true')
 
 # Create and print the training dataset
 train_dataset = dsets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
@@ -7,35 +13,48 @@ train_dataset = dsets.MNIST(root='./data', train=True, download=True, transform=
 validation_dataset = dsets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
 # print("Downloaded the validating dataset:\n ", validation_dataset)
 
+args = parser.parse_args()
 
-input_dimensions = 28*28
-output_dimensions = 10
+if(not args.use_trained_model):
 
-# Create a model
-model = SoftMax(input_dimensions, output_dimensions)
+	input_dimensions = 28*28
+	output_dimensions = 10
 
-# define an optimizer
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.1)
-# Define a loss function
-criterion = nn.CrossEntropyLoss()
-# Define dataloaders
-trainloader = DataLoader(dataset = train_dataset, batch_size = 100)
-validationloader = DataLoader(dataset = validation_dataset, batch_size = 5000)
+	# Create a model
+	model = SoftMax(input_dimensions, output_dimensions)
 
-PlotParameters(model)
-plt.title('Before Training')
+	# define an optimizer
+	optimizer = torch.optim.SGD(model.parameters(), lr = 0.1)
+	# Define a loss function
+	criterion = nn.CrossEntropyLoss()
+	# Define dataloaders
+	trainloader = DataLoader(dataset = train_dataset, batch_size = 100)
+	validationloader = DataLoader(dataset = validation_dataset, batch_size = 5000)
+
+	PlotParameters(model)
+	plt.title('Before Training')
 
 
-n_epochs = 100
-for epoch in range(n_epochs):
-	print('Running on epoch {}'.format(epoch), flush = True)
-	for x, y in trainloader:
-		optimizer.zero_grad()
-		z = model(x.view(-1, 28 * 28))
-		loss = criterion(z, y)
-		loss.backward()
-		optimizer.step()
-		
+	n_epochs = 100
+	for epoch in range(n_epochs):
+		print('Running on epoch {}'.format(epoch), flush = True)
+		for x, y in trainloader:
+			optimizer.zero_grad()
+			z = model(x.view(-1, 28 * 28))
+			loss = criterion(z, y)
+			loss.backward()
+			optimizer.step()
+
+	with open('model/trained_model.pkl', 'wb') as handle:
+    	pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+else:
+	if(not os.path.isfile('model/trained_model.pkl')):
+		print('Train the model first')
+		os._exit(1)
+
+	with open('model/trained_model.pkl', 'rb') as f:
+	    model = pickle.load(f)	
 
 PlotParameters(model)
 plt.title('After Training')
@@ -53,6 +72,7 @@ for (x,y) in validation_dataset:
 
 print("Analysis:")
 print("Correctly classified data count =", correct)
-print("Incorrectly classified data count=", incorrect)
+print("Incorrectly classified data count =", incorrect)
+print("Accuracy =", correct/(correct+incorrect))
 
 plt.show()
